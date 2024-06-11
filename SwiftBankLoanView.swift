d7d0e40b6cf02feb6ffab5b59e18042f1d3a4a66
rudrigocerqueira <rodrigocerqueirareis@gmail.com>
2024-06-11 08:05:03 -0300

    first commit

---
+++ SwiftBankLoanView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftBankLoanView.swift
+//  Pods
+//
+//  Created by Rodrigo Cerqueira Reis on 10/06/24.
+//
+
+import SwiftUI
+
+struct SwiftBankLoanView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftBankLoanView()
+}
